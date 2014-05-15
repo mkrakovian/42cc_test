ba2c924b039c157daccd85ceba239e3ef7289efb
mkrakovyan <max.krakovyan@gmail.com>
2014-05-15 21:21:35 +0300

    ticket1: Testing for dob presence on rendered page is implemented.

--- contact/tests.py
+++ contact/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from models import Person
+from django.conf import settings
+import datetime
 
 
 class HomeViewTest(TestCase):
@@ -16,5 +18,10 @@ class HomeViewTest(TestCase):
         # TODO: using the protected _meta, need to find another solution perhaps.
 
         for field in person._meta.fields:
-            if field.name != 'id' and field.name != 'birth':
-                self.assertContains(response, person.__getattribute__(field.name))
+            value = person.__getattribute__(field.name)
+            if type(value) == datetime.date:
+                formatting = settings.DATE_FORMAT.replace('N ', '%B').replace('j', '%e').replace('Y', '%Y')
+                self.assertContains(response, value.strftime(formatting))
+                pass
+            elif field.name != 'id':
+                self.assertContains(response, value)
