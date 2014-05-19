e5a1f2b2486845ebee79bbb27583b5b7f7136bf1
mkrakovyan <max.krakovyan@gmail.com>
2014-05-19 20:39:00 +0300

    ticket3: Middleware is implemented. Tests pass.

---
+++ contact/middleware.py
@@ -0,0 +1,12 @@
+from contact.models import Request
+
+
+class RequestSaveMiddleware(object):
+    def process_request(self, request):
+
+        Request.objects.create(path=request.path, method=request.method,
+                               encoding=request.META.get('HTTP_ACCEPT_ENCODING', 'N/A'),
+                               user_agent=request.META.get('HTTP_USER_AGENT', 'N/A'),
+                               ip=request.META.get('REMOTE_ADDR', 'N/A')
+                               )
+
--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,7 @@ from contact import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='home'),
-                       url(r'^person/$', ListView.as_view(queryset=Request.objects.order_by('id')[:10],
+                       url(r'^requests/$', ListView.as_view(queryset=Request.objects.order_by('id')[:10],
                                                           context_object_name='first_req_list',
                                                           template_name='contact/requests.html'), name='requests')
                        )
